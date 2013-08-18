e863500aa9bdcbc3e37c60efccbf5d6d425b6463
J-A-B-R <Super_Programador@yahoo.com>
2013-08-18 21:08:45 +0200

    Added copyright notices to all source code files

--- Elevate/Global.h
+++ Elevate/Global.h
@@ -1,3 +1,14 @@
+/* Copyright (c) 2013 Juan Burgos. All rights reserved.
+ *
+ * This source is subject to the Apache 2.0 License.
+ * Please see the included LICENSE file for more information.
+ * All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
 #pragma once
 
 #include "Constants.h"
