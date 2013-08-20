fcff293bcc0a746e4ea18f64235700af003d539b
J-A-B-R <Super_Programador@yahoo.com>
2013-08-20 08:23:20 +0200

    Changed the copyright header text in each file to be similar to that showed in GitHub

--- Elevate Stub/Global.c
+++ Elevate Stub/Global.c
@@ -1,12 +1,14 @@
-/* Copyright (c) 2013 Juan Burgos. All rights reserved.
+/* Copyright 2013 Juan Burgos. All rights reserved.
  *
- * This source is subject to the Apache 2.0 License.
- * Please see the included LICENSE file for more information.
- * All other rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
  *
- * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
- * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  */
 
 #include "stdafx.h"
